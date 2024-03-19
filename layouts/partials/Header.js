612eb16bfc01fd70c58c0353d24abb6c4c92dbd5
hirushaC <152847740+hirushaC@users.noreply.github.com>
2024-03-19 22:19:36 +0530

    changed header

--- layouts/partials/Header.js
+++ layouts/partials/Header.js
@@ -53,15 +53,15 @@ const Header = () => {
     <>
       <div className={`header-height-fix`}></div>
       <header
-        className={`header border-b-2 ${
-          sticky && "header-sticky"
-        } border-[#E2E4EB] bg-white`}
+        className={`header ${
+          sticky ? "header-sticky border-b-2 border-[#E2E4EB]" : ''
+        } `}
         ref={headerRef}
       >
         <nav className="navbar container">
           {/* logo */}
           <div className="order-0 flex items-start">
-            <Logo />
+            {sticky ? <Logo /> : <Logo src='/images/logo - white.svg'/>}
           </div>
 
           <ul
@@ -84,7 +84,7 @@ const Header = () => {
                     }
                   >
                     <span
-                      className={`nav-link inline-flex items-center text-[#424B66] ${
+                      className={`${sticky ? 'nav-link-sticky' : 'nav-link'} inline-flex items-center ${sticky ? 'text-[#424B66]' : 'text-white' } ${
                         showDropdown === i && "active"
                       }`}
                       onClick={() =>
@@ -104,7 +104,7 @@ const Header = () => {
                     {showDropdown === i && (
                       <div className="nav-dropdown-overlay top-full mx-12 w-full items-center justify-center bg-transparent p-2 lg:absolute lg:left-0 lg:mx-0">
                         <div
-                          className={`nav-dropdown-list divide-x-0 border border-[#E8F4FE] bg-white`}
+                          className={`nav-dropdown-list divide-x-0 bg-white  ${sticky && 'border border-[#E8F4FE]'}`}
                         >
                           <ul className="flex-auto py-2">
                             {/* Column 01 */}
@@ -163,7 +163,7 @@ const Header = () => {
                   <li className="nav-item">
                     <Link
                       href={menu.url}
-                      className={`nav-link block text-[#424B66] ${
+                      className={`nav-link block ${sticky ? 'text-[#424B66]' : 'text-white' } ${
                         asPath === menu.url && "active"
                       }`}
                     >
@@ -191,7 +191,7 @@ const Header = () => {
                 {/* <div className="pr-5 cursor-pointer hidden lg:block text-[#0368B1]"><TfiSearch /></div> */}
 
                 <Link
-                  className={`btn btn-primary hidden bg-primary opacity-100 lg:flex`}
+                  className={`btn hidden ${sticky ? 'bg-primary btn-primary' : 'bg-white btn-primary-sticky'} opacity-100 lg:flex`}
                   href={config.nav_button.link}
                 >
                   {config.nav_button.label}
@@ -221,7 +221,7 @@ const Header = () => {
                   height="32px"
                 >
                   <path
-                    fill={`black`}
+                    fill={`${sticky ? 'black' : 'white'}`}
                     d="M 5 5 L 5 11 L 11 11 L 11 5 L 5 5 z M 13 5 L 13 11 L 19 11 L 19 5 L 13 5 z M 21 5 L 21 11 L 27 11 L 27 5 L 21 5 z M 7 7 L 9 7 L 9 9 L 7 9 L 7 7 z M 15 7 L 17 7 L 17 9 L 15 9 L 15 7 z M 23 7 L 25 7 L 25 9 L 23 9 L 23 7 z M 5 13 L 5 19 L 11 19 L 11 13 L 5 13 z M 13 13 L 13 19 L 19 19 L 19 13 L 13 13 z M 21 13 L 21 19 L 27 19 L 27 13 L 21 13 z M 7 15 L 9 15 L 9 17 L 7 17 L 7 15 z M 15 15 L 17 15 L 17 17 L 15 17 L 15 15 z M 23 15 L 25 15 L 25 17 L 23 17 L 23 15 z M 5 21 L 5 27 L 11 27 L 11 21 L 5 21 z M 13 21 L 13 27 L 19 27 L 19 21 L 13 21 z M 21 21 L 21 27 L 27 27 L 27 21 L 21 21 z M 7 23 L 9 23 L 9 25 L 7 25 L 7 23 z M 15 23 L 17 23 L 17 25 L 15 25 L 15 23 z"
                   />
                 </svg>
