6331f532c1b3b97c54ae2fee253d73b9e0353ddf
hirushaC <152847740+hirushaC@users.noreply.github.com>
2024-03-20 11:26:40 +0530

    Updated Bentogrid responsiveness

--- layouts/components/BentoGrid.js
+++ layouts/components/BentoGrid.js
@@ -8,7 +8,7 @@ export const BentoGrid = ({ className, children }) => {
   return (
     <div
       className={cn(
-        "mx-auto grid grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3",
+        "mx-auto grid grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-2 lg:grid-cols-3",
         className
       )}
     >
